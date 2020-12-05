f6d30585bc068a8b70e7a1e7a360ecfdffe93b0a
nandoputrap <nandoputrap@gmail.com>
2020-12-05 23:07:38 +0700

    add procfile and env port

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post("/failure", (req, res) => {
     res.redirect("/");
 });
 
-app.listen(port, () => {
+app.listen( process.env.PORT || port, () => {
     console.log(`App listening at http://localhost:${port}`);
 });
 
